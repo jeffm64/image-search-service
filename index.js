44054fc286ad96a11ae82c2241256ce92aca3852
Jeff <jeff_m_england@yahoo.com>
2016-11-23 19:41:07 +0000

    setup server to run

---
+++ index.js
@@ -0,0 +1,8 @@
+var app = require('./src/app').app;
+var express = require("express");
+
+var app = express();
+
+app.listen(process.env.PORT || 8080, function () {
+  console.log("App listening on port 8080!");
+});
